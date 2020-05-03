fd5f4faef7526fffde1eda75db52c6846113a251
HongTat <ht_yew@hotmail.com>
2020-05-03 19:35:57 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let { binarySearch } = require('./search-algorithms/binary-search');
+
+(async () => {
+    // let sortedArray = [1, 3, 4, 6, 10, 12, 15];
+    // let searchValue = 10;
+    // let searchResult = await binarySearch(sortedArray, searchValue);
+    // console.log('Binary search');
+    // console.log(searchResult); // 4
+
+    // searchValue = 30;
+    // console.log(searchResult); // -1
+
+    
+})();
---
+++ src/search-algorithms/binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param {Array} sortedArray Sorted array to search
+ * @param {any} value Value to search
+ * @returns {int} Index of value. Return -1 if value is not found.
+ */
+exports.binarySearch = async (sortedArray, value) => {
+    let firstIndex = 0;
+    let lastIndex = sortedArray.length - 1;
+
+    while (firstIndex <= lastIndex) {
+        let middleIndex = parseInt((lastIndex - firstIndex) / 2) + firstIndex;
+        if (sortedArray[middleIndex] === value) {
+            return middleIndex;
+        } else if (sortedArray[middleIndex] < value) {
+            firstIndex = middleIndex + 1;
+        } else {
+            lastIndex = middleIndex - 1;
+        }
+    }
+    return -1; // not found
+}
